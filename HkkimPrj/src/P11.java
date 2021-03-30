1747f42af72392a0bd63f3d6bbde615d5c9dba93
Hakkyeom-Kim <khkcar@naver.com>
2021-03-30 19:57:35 +0900

    practice 11

---
+++ HkkimPrj/src/P11.java
@@ -0,0 +1,22 @@
+
+public class P11 {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i<3;i++)
+		{
+			System.out.printf("안녕하세요?\n");
+			System.out.printf("##또 안녕하세요?##\n");
+			
+		}
+		
+		System.out.printf("\n\n");
+		
+		for (i=0; i<3; i++)
+			System.out.printf("안녕하세요?\n");
+		System.out.printf("##또 안녕하세요?##\n");
+		
+		
+	}
+
+}
